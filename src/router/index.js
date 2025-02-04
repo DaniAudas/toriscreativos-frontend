63792209f2f58a4dc81d3a0f610adaaec91413ac
pumas <pumasrebeld@gmail.com>
2025-02-04 00:31:45 -0600

    modificacion de router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import PrincipalView from "../views/PrincipalView.vue";
+import MesaView from "../views/MesaView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
         {
             path: '/mesa',
             name: 'mesa',
-            component: import('../views/MesaView.vue'),
+            component: MesaView
         }
     ]
 })
