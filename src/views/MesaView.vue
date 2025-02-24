34845b50980a87f92f6a1748be3d626e34696a3e
pumas <pumasrebeld@gmail.com>
2025-02-23 20:46:25 -0600

    Modificar mesa de regalo

--- src/views/MesaView.vue
+++ src/views/MesaView.vue
@@ -2,7 +2,7 @@
   <div class="bg-[#B3A500] flex flex-col items-center justify-center w-full">
     <img src="/img/margen-corazon.png" class="w-4/5 h-4/5 md:w-1/2 md:h-1/2 mt-10 mb-10" />
     <h3 class="text-4xl text-center text-[#162651] mb-10 font-bold mx-10">
-      Su compañia es el mejor obsequio que podríamos recibir. Sin enbargo,
+      Su compañia es el mejor obsequio que podríamos recibir. Sin embargo,
       si desean tener un detalle con nosotros, les dejamos algunas sugerencias.
     </h3>
     <a href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51527423" target="_blank" class="w-4/5 h-4/5 md:w-1/2 md:h-1/2 mt-10 mb-10 ">
@@ -11,7 +11,6 @@
     <a href="https://www.sears.com.mx/Mesa-de-Regalos/177816/te-invito-a-mi-boda-victor-hugo-diana-laura" target="_blank" class="w-4/5 h-4/5 md:w-1/2 md:h-1/2 mt-10 mb-10 ">
       <img src="/img/sears-mesa-de-regalos.jpg" class="rounded-md hover:opacity-70" />
     </a>
-    <h3 class="text-3xl text-[#162651] font-bold mx-10 text-center"> O un sobre con dinero 😅</h3>
     <img src="/img/sobre-regalo.jpg" alt="Sobre con Dinero" class="w-4/5 h-4/5 md:w-1/3 md:h-1/3 mt-10 mb-10 rounded-md" />
   </div>
 </template>
