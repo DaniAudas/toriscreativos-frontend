96a346a808177f548c66a6eaa37a6eb60c76ba57
pumas <pumasrebeld@gmail.com>
2025-06-21 01:55:22 -0600

    Correxión texto botones

--- src/components/Direcciones.vue
+++ src/components/Direcciones.vue
@@ -40,7 +40,7 @@ export default {
           <br><span class="underline">Hora:</span> 4:00 pm
         </h3>
         <div class="mt-6">
-          <a href="https://maps.app.goo.gl/2NJEBwJCWrY9kiJC8" target="_blank" class="text-white py-2 px-6 bg-blue-600 hover:bg-blue-700 mt-6 shadow-md hover:shadow-xl">Dirigime</a>
+          <a href="https://maps.app.goo.gl/2NJEBwJCWrY9kiJC8" target="_blank" class="text-white py-2 px-6 bg-blue-600 hover:bg-blue-700 mt-6 shadow-md hover:shadow-xl">Uicación</a>
         </div>
       </div>
     </div>
@@ -62,7 +62,7 @@ export default {
           <br><span class="underline">Hora:</span> 7:00 pm
         </h3>
         <div class="mt-6 md:mb-6 lg:mb-0 relative">
-          <a href="https://maps.app.goo.gl/EJLjw4rJMfDpsdgz9" target="_blank" class="text-white py-2 px-6 bg-blue-600 hover:bg-blue-700 mt-6 shadow-md hover:shadow-xl transition-shadow duration-300">Dirigime</a>
+          <a href="https://maps.app.goo.gl/EJLjw4rJMfDpsdgz9" target="_blank" class="text-white py-2 px-6 bg-blue-600 hover:bg-blue-700 mt-6 shadow-md hover:shadow-xl transition-shadow duration-300">Ubicación</a>
         </div>
       </div>
     </div>
